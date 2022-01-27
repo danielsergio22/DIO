4eff93164b85f17c4f161a813460edfcb6101a68
danielsergio22 <ds.daniel.sergio@gmail.com>
2022-01-27 09:29:18 -0300

    desafio banco digital - NuBank

---
+++ Desafios/LabBancoDigital/src/ContaPoupanca.java
@@ -0,0 +1,14 @@
+package DIO.Desafios.LabBancoDigital.src;
+
+public class ContaPoupanca extends Conta {
+
+	public ContaPoupanca(Cliente cliente) {
+		super(cliente);
+	}
+
+	@Override
+	public void imprimirExtrato() {
+		System.out.println("=== Extrato Conta Poupan�a ===");
+		super.imprimirInfosComuns();
+	}
+}

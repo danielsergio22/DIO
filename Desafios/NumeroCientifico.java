12134e894104eb3be799388fa90df4e6f5bb8acd
danielsergio22 <ds.daniel.sergio@gmail.com>
2022-02-15 22:46:31 -0300

    desafios

---
+++ Desafios/NumeroCientifico.java
@@ -0,0 +1,18 @@
+package DIO.Desafios;
+import java.util.Scanner;
+
+public class NumeroCientifico {
+    public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		double X = scan.nextDouble();
+		
+		if(X > 0){ // aqui é o que separa numero positivo do negativo e imprime
+		System.out.printf("+%.4E", X );
+		} else {
+		System.out.printf("%.4E", X );
+
+        scan.close();
+	}
+}
+}

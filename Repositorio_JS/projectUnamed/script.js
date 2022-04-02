baf05dae5aa2edccf66bd7b0684d391e23a94105
danielsergio22 <ds.daniel.sergio@gmail.com>
2022-04-02 15:12:47 -0300

    testando variaveis e objetos

---
+++ Repositorio_JS/projectUnamed/script.js
@@ -0,0 +1,9 @@
+var objetoTeste = {
+    atributo1: 2, 
+    atributo2: "as",
+}
+
+variavel1 = 2
+funcao1 = [objetoTeste, 1, true]
+var objetoteste2 = {variavel1, funcao1};
+console.log(objetoteste2)

dcf5538166634863cb571a1ef4fbf6bf780f4209
danielsergio22 <ds.daniel.sergio@gmail.com>
2022-05-02 23:47:28 -0300

    error handling

--- JS/projectUnamed/script.js
+++ JS/projectUnamed/script.js
@@ -1,12 +1,32 @@
-const funcao = (arr,num) =>{
+function validaArray(arr,num){
     try{
+        if (!arr && !num) throw new ReferenceError("Envie os parâmetros");
+
+        if (typeof(arr) != Object) 
+        throw new TypeError("Array precisa ser do tipo object");
         
-        if(typeof(arr) != Object || typeof(num) != Number) {
-            console.log(TypeError);
-        }
+        if (typeof(num) != Number) 
+        throw new TypeError("Num precisa ser do tipo number");
+        
+        if(arr.length !== num) throw new RangeError("Tamanho inválido!");
+
+        return arr;
     } catch(e){
+        if (e instanceof ReferenceError) {
+            console.log("Este erro é ReferenceError!");
+            console.log(e.message);
+        } else if (e instanceof TypeError) {
+            console.log("Este erro é TypeError!");
+            console.log(e.message);
+        } else if (e instanceof RangeError) {
+            console.log("Este erro é RangeError!");
+            console.log(e.message);
+        } else {
+            console.log("Tipo de erro não esperado:" + e);
+        }
+        
     } finally{
-        console.log(`sua array e numero é: ${arr} ${num}`);
+        console.log(`os dados recebidos foram: ${arr} | ${num}`);
     }
 }
-funcao("",8);
+console.log(validaArray([1,2,3],3));
